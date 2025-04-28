72a02299d12e253d2b896658ba7d8301c8ff7b88
yakiv <yakov.manitskiy@gmail.com>
2025-04-28 23:05:17 +0300

    client: added tailwind, removed extra files

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
