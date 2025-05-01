6b67ab181019152e610d0c95a4957b67be6a37e4
yakiv <yakov.manitskiy@gmail.com>
2025-05-01 21:42:51 +0300

    server: added user to DB with registration, added auth

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 import {validationResult} from "express-validator";
 import {registerValidation} from "./validations/auth.js";
+import userModel from "./models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -22,16 +25,94 @@ app.get('/', (req, res) => {
   res.json({ message: 'Hello world' });
 });
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req);
+app.post('/auth/register', registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = userModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash
+    });
+
+    const user = await doc.save();
+
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      process.env.TOKEN_SECRET_KEY,
+      {
+        expiresIn: '30d'
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Registration failed'
+    });
   }
 
-  res.json({
-    success: true,
-  })
+});
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Wrong email or password'
+      });
+    }
+
+    const isValidStatus = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if(!isValidStatus) {
+      return res.status(404).json({
+        message: 'Wrong email or password'
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id
+      },
+      process.env.TOKEN_SECRET_KEY,
+      {
+        expiresIn: '30d'
+      }
+    )
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token
+    });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Auth failed'
+    });
+  }
 });
 
 export const viteNodeApp = app;
