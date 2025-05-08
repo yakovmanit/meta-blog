9f873ace8bbadc605d6eb8c794fa1744e9467ced
yakiv <yakov.manitskiy@gmail.com>
2025-05-08 22:25:38 +0300

    client: add PostCard component, add Posts section

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import banner from '../assets/img/banner.jpg';
 import heroAvatar from '../assets/img/hero-avatar.jpg'
+import PostCard from "../components/PostCard.tsx";
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div className="mb-48">
       {/* Hero */}
-      <section className="mt-6">
+      <section className="mt-6 mb-16 md:mb-36">
         {/* Hero wrapper */}
-        <div className="relative">
+        <div className="wrapper relative">
           <div className="h-[300px] rounded-xl overflow-hidden md:h-[600px]">
             <img className="h-full w-full object-cover" src={banner} alt="banner"/>
           </div>
@@ -18,12 +19,26 @@ const Home: React.FC = () => {
             <div className="flex items-center text-gray">
               <img className="rounded-full mr-3 w-8 h-8" src={heroAvatar} alt="avatar"/>
               <p className="mr-5 font-medium">Jason Francisco</p>
-              <time className="">August 20, 2022</time>
+              <time>August 20, 2022</time>
             </div>
           </div>
         </div>
       </section>
-    </>
+
+      {/* Posts */}
+      <section>
+        <h2 className="mb-6 text-xl font-bold md:text-2xl md:mb-8">
+          Latest Post
+        </h2>
+
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </div>
+      </section>
+    </div>
   );
 };
 
