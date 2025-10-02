9550ab1feba401d86d28e359a5ffaf7d3b6f9566
yakiv <yakov.manitskiy@gmail.com>
2025-10-03 00:32:36 +0300

    chore(posts): delete post thunk

--- client/redux/api/postApi.ts
+++ client/redux/api/postApi.ts
@@ -14,6 +14,8 @@ const postApi = api
         }),
       }),
 
+
+
     }),
   });
 
--- client/redux/slices/postsSlice.ts
+++ client/redux/slices/postsSlice.ts
@@ -1,18 +1,6 @@
 import {createAsyncThunk, createSlice} from '@reduxjs/toolkit'
 import axios from "../../src/axios";
 import {PostType} from "../../src/types.ts";
-// import type { PayloadAction } from '@reduxjs/toolkit'
-
-export const fetchPosts = createAsyncThunk('posts/getAll', async () => {
-    try {
-      const { data } = await axios.get('/posts');
-
-      return data;
-
-    } catch (err) {
-      console.log(err);
-    }
-})
 
 export const deletePost = createAsyncThunk('posts/delete', async (id: string) => {
     try {
@@ -42,20 +30,6 @@ export const postsSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      // Posts fetching
-      .addCase(fetchPosts.pending, (state) => {
-        state.items = [];
-        state.loading = 'pending';
-      })
-      .addCase(fetchPosts.fulfilled, (state, action) => {
-        state.items = action.payload;
-        state.loading = 'succeeded';
-      })
-      .addCase(fetchPosts.rejected, (state) => {
-        state.items = [];
-        state.loading = 'failed';
-      })
-
       // Post deleting
       .addCase(deletePost.fulfilled, (state, action) => {
         state.items = state.items.filter(post => post._id !== action.meta.arg);
