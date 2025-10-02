b2b5b18cc8200ada40d7c0be93bb84370ec56bc2
yakiv <yakov.manitskiy@gmail.com>
2025-10-02 22:41:39 +0300

    chore: add RTK Query API configuration

---
+++ client/src/api/api.ts
@@ -0,0 +1,36 @@
+import {
+  BaseQueryFn,
+  createApi,
+  FetchArgs,
+  fetchBaseQuery,
+  FetchBaseQueryError,
+} from '@reduxjs/toolkit/query/react';
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${process.env.PUBLIC_API_URL}`,
+
+  prepareHeaders: (headers) => {
+    const token = localStorage.getItem('token');
+
+    if (token) headers.set('Authorization', `Bearer ${token}`);
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
+
+  if (result.error && result.error.status === 401) {
+    window.location.href = '/login';
+  }
+
+  return result;
+};
+
+export const api = createApi({
+  baseQuery: baseQueryWithReauth,
+  endpoints: () => ({}),
+});
